af5056bf39528d6a20304e6f3936ad1f13e11da0
Shaowei <Shaowei.Zhang@Outlook.com>
2021-09-23 18:33:54 +1000

    ngClass to update style

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,7 +3,12 @@ import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-server',  //must be unique
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+    .online {
+      color: white;
+    }
+  `]
 })
 
 
